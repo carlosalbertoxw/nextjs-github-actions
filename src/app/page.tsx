209ee021cd065b2e2c6598bb88737444ff8bcfbc
Carlos Alberto <carlosalbertoxw@gmail.com>
2026-01-25 18:45:34 -0600

    Se corrige la ruta de las imágenes.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
           className="dark:invert"
-          src="/next.svg"
+          src="next.svg"
           alt="Next.js logo"
           width={100}
           height={20}
@@ -43,7 +43,7 @@ export default function Home() {
           >
             <Image
               className="dark:invert"
-              src="/vercel.svg"
+              src="vercel.svg"
               alt="Vercel logomark"
               width={16}
               height={16}
